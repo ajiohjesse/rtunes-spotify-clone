26560be89ce9bf4c329b99d151ec1de218de0ab0
Jesse Ajioh <ajiohjesse@gmail.com>
2024-06-16 12:56:28 +0100

    feat: get and display songs in library

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,13 @@
+import getSongs from '@/actions/getSongs';
 import Header from '@/components/header';
 import ListItem from '@/components/list-item';
+import PageContent from '@/components/page-content';
 
-export default function Home() {
+export const revalidate = 0;
+
+export default async function Home() {
+  const songs = await getSongs();
+
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
@@ -23,7 +29,7 @@ export default function Home() {
           <h2 className="text-white text-2xl font-semibold">Newest songs</h2>
         </div>
 
-        <div>List of songs!</div>
+        <PageContent songs={songs} />
       </div>
     </div>
   );
