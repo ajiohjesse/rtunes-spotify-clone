e4ac60091098aaa414f3b287e01d3eb1bc4f8e16
Jesse Ajioh <ajiohjesse@gmail.com>
2024-06-17 00:37:45 +0100

    feat: add sound player functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import UserProvider from '@/providers/user-provider';
 import ModalProvider from '@/providers/modal-provider';
 import ToasterProvider from '@/providers/toaster-provider';
 import getSongsByUserId from '@/actions/getSongsByUseId';
+import Player from '@/components/player';
 
 const font = Figtree({ subsets: ['latin'] });
 
@@ -31,6 +32,7 @@ export default async function RootLayout({
           <UserProvider>
             <ModalProvider />
             <Sidebar songs={userSongs}>{children}</Sidebar>
+            <Player />
           </UserProvider>
         </SupabaseProvider>
       </body>
